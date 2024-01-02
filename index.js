3c2d4d00a9218eff0221effd7369dab48c25373b
codeshaine <shainilps05@gmail.com>
2024-01-02 17:13:47 +0530

    done with api might add db later

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ import express from "express";
 import jokes from "./data/apiData.js";
 const app = express();
 const port = 3000;
-const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
+const masterKey = "codeshaine@123";
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/jokes/all", (req, res, next) => {
+  if (req.query.key == masterKey) {
+    next();
+  } else {
+    res
+      .status(404)
+      .json({ status: "failed", message: "You are not Authorized" });
+  }
+});
+
 //1. GET a random joke
 app.get("/random", (req, res) => {
-  const randomNumber = Math.floor(Math.random() * jokes.length);
-  const data = jokes[randomNumber];
-  res.json({ status: "success", message: "random joke", data: data });
+  try {
+    if (jokes.length === 0) throw new Error("The database is empty");
+    const randomNumber = Math.floor(Math.random() * jokes.length);
+    const data = jokes[randomNumber];
+    res.json({ status: "success", message: "random joke", data: data });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      message: `Error occured: ${err}`,
+    });
+  }
 });
 
 //2. GET a specific joke
@@ -70,36 +88,90 @@ app.put("/jokes/:id", (req, res) => {
   const missingproperties = expectedProperties.filter(
     (props) => !(props in req.body)
   );
-  if (missingproperties.length === 0) {
+  try {
+    const index = jokes.findIndex((item) => item.id === id);
+    if (index === -1) throw new Error("index not found");
+    if (missingproperties.length !== 0)
+      throw new Error("check the properties entered");
     const replaceItem = {
       id: id,
       jokeText: req.body.text,
       jokeType: req.body.type,
     };
-    const index = jokes.findIndex((item) => item.id === id);
+
     jokes[index] = replaceItem;
     res.json({
       status: "success",
       message: "successfully updated",
       data: replaceItem,
     });
-  } else {
+  } catch (err) {
     res.status(404).json({
       status: "failed",
-      message: `Invalid id ${id}/missing properties ${missingproperties}`,
+      message: `Error occured:${err}`,
     });
   }
 });
 
 //6. PATCH a joke
-app.patch("/jokes/:id", (req, res) => {});
-//7. DELETE Specific joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const existingItem = jokes.find((item) => item.id === id);
+    const replaceItem = {
+      id: id,
+      jokeText: req.body.text || existingItem.jokeText,
+      jokeType: req.body.type || existingItem.jokeType,
+    };
+    const index = jokes.findIndex((item) => item.id === id);
+    if (index === -1) throw new Error("index not found");
+    jokes[index] = replaceItem;
+    res.json({
+      status: "success",
+      message: "successfully updated",
+      data: replaceItem,
+    });
+  } catch (err) {
+    res.status(403).json({
+      status: "failed",
+      message: `Error occured: ${err}`,
+    });
+  }
+});
+// 7. DELETE All jokes
+app.delete("/jokes/all", (req, res) => {
+  try {
+    jokes.splice(0, jokes.length);
+    res.json({
+      status: "success",
+      message: `successfully deleted all details`,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      message: `error occured:${err}`,
+    });
+  }
+});
 
-//8. DELETE All jokes
-// app.get("jokes/all", (req, res) => {
-//   if (jokes.length == 0) res.send(404).json("there is no data found");
-//   else res.json(jokes);
-// });
+//8. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const index = jokes.findIndex((item) => item.id === id);
+    if (index === -1) throw new Error("index not found");
+    jokes.splice(index, 1);
+    res.json({
+      status: "success",
+      message: `successfully deleted id: ${id}`,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      message: `Error occured: ${err}`,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
